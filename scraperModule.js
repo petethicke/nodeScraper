f74728dc6316e0a2a8b2fa7d6b8b539e235f0a8b
Peter Thicke <peterbthicke@gmail.com>
2015-05-13 02:14:59 +0000

    url and permission scraping functional

--- scraperModule.js
+++ scraperModule.js
@@ -1,10 +1,18 @@
+var fs = require('fs');
 var cheerio = require('cheerio') 
 var request = require('request')
-var url = "http://substack.net/images/"
 
-request(url, function (error, response, body) {
-  if (!error) {
-    console.log('success!');
+
+// GET Request
+request("http://substack.net/images/", function (error, response, body) {
+  if (!error && response.statusCode == 200) {
+    console.log('working');
+    var $ = cheerio.load(body);
+    $("tr").each(function(i, element) {
+      var permission = $(this).children().first().text();
+      var url = "http://substack.net/images" + $(this).find('a').attr('href');
+      console.log("Permission: " + permission + ' || ' + "URL: " + url);
+    });
   } else {
     console.log('We have an error: ' + error);
   }

d1e00df30858d3b5919866627f1e490e56d831c3
Peter Thicke <peterbthicke@gmail.com>
2015-05-13 02:27:55 +0000

    File type scrape working

--- scraperModule.js
+++ scraperModule.js
@@ -1,6 +1,7 @@
 var fs = require('fs');
 var cheerio = require('cheerio') 
 var request = require('request')
+var path = require('path')
 
 
 // GET Request
@@ -11,7 +12,8 @@ request("http://substack.net/images/", function (error, response, body) {
     $("tr").each(function(i, element) {
       var permission = $(this).children().first().text();
       var url = "http://substack.net/images" + $(this).find('a').attr('href');
-      console.log("Permission: " + permission + ' || ' + "URL: " + url);
+      var type = path.extname(url);
+      console.log("Permission: " + permission + ' || ' + "URL: " + url + ' || ' + "File Type: " + type);
     });
   } else {
     console.log('We have an error: ' + error);
